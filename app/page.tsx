5e9fbbfc96d8a625cedc548e3f82840a05d250a7
Xingbin Gao <xingbingao@gmail.com>
2025-03-18 23:21:29 +1300

    Improve mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,7 @@ import { sourceCodePro400, sourceCodePro700 } from '../styles/fonts';
 const Section = ({ id, title, children }: { id: string; title: string; children: React.ReactNode }) => (
   <section
     id={id}
-    className={`${sourceCodePro400.className} relative z-10 bg-white p-8 py-48 rounded-lg shadow-md mx-8 sm:mx-16 lg:mx-32 min-h-[10rem]`}
-    style={{ margin: '2rem' }}
+    className={`${sourceCodePro400.className} relative z-10 bg-white p-4 sm:px-4 md:px-8 lg:px-16 sm:py-4 md:py-12 lg:py-24 rounded-lg shadow-md m-4 sm:m-4 md:m-8 lg:mx-16 min-h-[10rem]`}
   >
     <h2 className='text-2xl sm:text-3xl lg:text-4xl font-semibold mb-6'>{title}</h2>
     {children}
@@ -78,17 +77,17 @@ export default function Home() {
   return (
     <div>
       <canvas ref={canvasRef} className='fixed top-0 left-0 w-full h-full -z-10'></canvas>
-      <div className="relative min-h-screen bg-transparent text-black font-['Playfair_Display'] p-6 sm:p-12 lg:p-20 space-y-10">
+      <div className="relative min-h-screen bg-transparent text-black font-['Playfair_Display'] p-0 sm:p-0 lg:px-20 space-y-0">
         {/* Header */}
         <header
           className={`${
             sourceCodePro700.className
-          } fixed top-0 left-0 w-full bg-white p-4 flex items-center z-50 shadow-md transition-transform duration-300 ${
+          } fixed top-0 left-0 w-full bg-white px-8 py-6 sm:py-6 md:py-4 flex items-center z-50 shadow-md transition-transform duration-300 ${
             isScrollingUp ? 'translate-y-0 ease-in' : '-translate-y-full ease-out'
           }`}
         >
-          <h1 className='text-2xl sm:text-4xl font-bold absolute left-1/2 transform -translate-x-1/2'>Ben Gao</h1>
-          <div className='header-icons flex gap-4 ml-auto'>
+          <h1 className='text-2xl font-bold absolute left-1/2 transform -translate-x-1/2'>Ben Gao</h1>
+          <div className='header-icons hidden sm:flex gap-4 ml-auto'>
             <button
               onClick={() => scrollToSection('personal-info')}
               className='w-8 h-8 rounded-full flex items-center justify-center cursor-pointer'
@@ -125,16 +124,20 @@ export default function Home() {
         <Section id='skills' title=''>
           <div className='flex flex-wrap gap-4'>
             {[
-              'React',
+              'React.js',
               'TypeScript',
               'JavaScript',
-              'HTML',
-              'CSS',
               'Tailwind CSS',
               'Next.js',
               'Node.js',
-              'Git',
-              'REST APIs',
+              'GCP',
+              'AWS',
+              'Firebase',
+              'Message queue',
+              'Web authentication',
+              'Mobile-first design',
+              'MongoDB',
+              'Terraform',
             ].map((skill, index) => (
               <span key={index} className='px-4 py-2 bg-gray-200 text-sm sm:text-base rounded-full shadow-md'>
                 {skill}
@@ -145,23 +148,63 @@ export default function Home() {
 
         {/* Qualifications Section */}
         <Section id='qualifications' title=''>
-          <ul className='list-disc pl-4 sm:pl-6'>
-            <li className='text-base sm:text-lg'>10+ years of experience in frontend development</li>
-            <li className='text-base sm:text-lg'>Proficient in React, Tailwind CSS, and TypeScript</li>
+          <ul className='list-disc'>
+            <li className='block mb-4 text-base sm:text-lg'>
+              Cloud and Backend Expertise: Proficient in GCP, Node.js, Express.js, Docker, serverless architectures (AWS
+              Lambda, Firebase), and CI/CD pipelines (CircleCI, GitHub Actions).
+            </li>
+
+            <li className='block mb-4 text-base sm:text-lg'>
+              Frontend Mastery: Extensive experience with React.js, TypeScript, and modern CSS practices
+              (Styled-components, Tailwind.css), focusing on pixel-perfect UI/UX implementation.
+            </li>
+
+            <li className='block mb-4 text-base sm:text-lg'>
+              UI/UX Design and Development: Skilled in translating designs from tools like Figma and Sketch into
+              functional web applications, with a strong understanding of responsive design and UI frameworks
+              (Bootstrap, Material Design, Ant Design).
+            </li>
+
+            <li className='block mb-4 text-base sm:text-lg'>
+              Testing and Quality Assurance: Experienced in setting up Jest testing frameworks, writing unit and
+              end-to-end tests, and implementing snapshot testing for React.js components, including blockchain testing.
+            </li>
+
+            <li className='block mb-4 text-base sm:text-lg'>
+              Blockchain and Open Source Contributions: Hands-on experience with Substrate and Polkadot.js, contributing
+              to open-source projects, managing CI/CD, and conducting code reviews.
+            </li>
+
+            <li className='block mb-4 text-base sm:text-lg'>
+              Agile and Web Management: Practical experience in Agile methodologies (Scrum), website management (DNS,
+              Cloudflare), and business website development, including SEO and Google Analytics.
+            </li>
           </ul>
         </Section>
 
         {/* Personal Projects Section */}
         <Section id='projects' title=''>
           <div className='flex flex-col gap-8'>
-            {['Project 1', 'Project 2', 'Project 3'].map((project, index) => (
-              <div key={index} className='p-4 w-full'>
-                <h3 className='text-lg sm:text-xl font-medium mb-2'>{project}</h3>
-                <p className='text-sm sm:text-base'>
-                  This is a description for {project}. It highlights the key features and technologies used.
-                </p>
-              </div>
-            ))}
+            <div key={1} className='p-4 w-full'>
+              <h3 className='text-lg sm:text-xl font-medium mb-2'>react-retheming-theme</h3>
+              <p className='text-sm sm:text-base'>
+                A utility of CSS framework integration in styled-components -
+                https://github.com/benxgao/react-retheming-theme
+              </p>
+            </div>
+            <div key={2} className='p-4 w-full'>
+              <h3 className='text-lg sm:text-xl font-medium mb-2'>strapi-terraform-gcp-sample</h3>
+              <p className='text-sm sm:text-base'>
+                A template for deploying Strapi CMS on GCP with Terraform and CircleCI -
+                https://github.com/benxgao/strapi-terraform-gcp-sample
+              </p>
+            </div>
+            <div key={3} className='p-4 w-full'>
+              <h3 className='text-lg sm:text-xl font-medium mb-2'>benxgao-next</h3>
+              <p className='text-sm sm:text-base'>
+                My profile website built with Github Copilot and Next.js - https://github.com/benxgao/benxgao-next
+              </p>
+            </div>
           </div>
         </Section>
 
@@ -195,50 +238,52 @@ export default function Home() {
 
       {/* Footer */}
       <footer
-        className={`${sourceCodePro400.className} h-60 w-screen bg-black flex items-center justify-between px-24 py-24 relative`}
+        className={`${sourceCodePro400.className} w-screen bg-black px-4 sm:px-4 md:px-12 lg:px-24 py-12 relative`}
       >
-        <div className='flex flex-col gap-2'>
-          {['x.com'].map((site, index) => (
+        <div className='flex mb-6'>
+          <div className='flex flex-col gap-2 mr-4'>
+            {['X'].map((site, index) => (
+              <a
+                key={index}
+                href='https://x.com/benxgao'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-white hover:text-gray-400 text-sm sm:text-base'
+              >
+                {site}
+              </a>
+            ))}
+          </div>
+          <div className='flex gap-4'>
             <a
-              key={index}
+              href='https://linkedin.com/in/benxgao'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-white hover:text-sky-400'
+            >
+              <FaLinkedin size={24} />
+            </a>
+            <a
+              href='https://github.com/benxgao'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-white hover:text-fuchsia-400'
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
               href='https://x.com/benxgao'
               target='_blank'
               rel='noopener noreferrer'
-              className='text-white hover:text-gray-400 text-sm sm:text-base'
+              className='text-white hover:text-amber-300'
             >
-              {site}
+              <FaFacebook size={24} />
             </a>
-          ))}
+          </div>
         </div>
-        <div className='flex gap-4'>
-          <a
-            href='https://linkedin.com/in/benxgao'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-white hover:text-gray-400'
-          >
-            <FaLinkedin size={24} />
-          </a>
-          <a
-            href='https://github.com/benxgao'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-white hover:text-gray-400'
-          >
-            <FaGithub size={24} />
-          </a>
-          <a
-            href='https://x.com/benxgao'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-white hover:text-gray-400'
-          >
-            <FaFacebook size={24} />
-          </a>
-        </div>
-        <div className='absolute bottom-14 left-1/2 transform -translate-x-1/2 text-center'>
-          <p className='text-sm sm:text-base text-white'>© 2025 benxgao.com</p>
-          <p className='text-sm sm:text-base text-white'>Built with GitHub Copilot & Next.js</p>
+        <div className='w-full absolute left-1/2 transform -translate-x-1/2 text-center'>
+          <p className='text-sm text-white'>copyright © 2025 benxgao.com</p>
+          <p className='text-sm text-white'>Built with GitHub Copilot & Next.js</p>
         </div>
       </footer>
     </div>
